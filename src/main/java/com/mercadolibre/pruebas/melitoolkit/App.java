9180ed4c5cf14d8d15aa392a0b9e46d083b88f45
Ignacio Alvarez <ignacio.alvarez@mercadolibre.com>
2019-01-07 16:48:44 -0300

    Initial commit

---
+++ src/main/java/com/mercadolibre/pruebas/melitoolkit/App.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.pruebas.melitoolkit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
